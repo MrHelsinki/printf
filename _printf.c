271b6a991a21e58900d338378729da0ade16c19d
MrHelsinki <taha.chad2001@gmail.com>
2023-11-11 05:51:12 +0100

    auto push script

--- _printf.c
+++ _printf.c
@@ -15,9 +15,7 @@ int _printf(const char *format, ...)
 	len = 0;
 	va_start(args, format);
 
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
+	if (format == NULL)
 		return (-1);
 
 	while (i < _strlen(format))
