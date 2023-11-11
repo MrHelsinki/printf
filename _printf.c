da940b57afb2febbe7e6f4fab29787aca8ff7b28
MrHelsinki <taha.chad2001@gmail.com>
2023-11-11 10:49:24 +0100

    auto push script

--- _printf.c
+++ _printf.c
@@ -16,6 +16,8 @@ int _printf(const char *format, ...)
 
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
 
 	while (*format)
 	{
